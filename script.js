5259bb36f03c6a2cba90af0eab1eea3e145f2e15
emmya <emmy.armintrout@gmail.com>
2014-09-30 22:12:54 -0700

    animate dog, decrease size, update links

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
 
-var dawg = new Image();
-dawg.src = "DogGuitarGif.gif";
+$(document).ready(function(){
+    // $('header').mouseenter(function() {
+    //     $('header').fadeTo('fast', .6);
+    // });
+    // $('header').mouseleave(function() {
+    // 	$('header').fadeTo('fast', 1);
+    // });
+    $('button').mouseenter(function() {
+    	$(this).fadeTo('fast', .2);
+    });
+    $('button').mouseleave(function() {
+    	$(this).fadeTo('fast', 1);
+    });
+    var count=0;
+    $('#dog').click(function() {
+    	crazyDog(count);
 
-function goDog() {
-	document.getElementById("doog") = dawg;
+    // 	$('#jamdog').html('<img src="DogGuitarGif.gif">');
+    // 	animateDiv();
+    // 	$('.whoa').html('stop it dog!');
+    // 	$('#dog').addClass('stop');
+    // });
+    // $('#dog').click(function() {
+    // 	$('#jamdog').toggle();
+    });
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
 }
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    $('#jamdog').animate({ top: newq[0], left: newq[1] }, 2000, function(){
+      animateDiv();        
+    });
+};
+
+function crazyDog(count) {
+	if (count%2 === 0) {
+		$('#jamdog').html('<img src="DogGuitarGif.gif" height=200px width=170px>');
+		animateDiv();
+		$('.whoa').html("can't stop the dog!");
+		count++;
+	} else {
+		$('.whoa').html('you cant stop this dog!');
+		count++;
+	}
+};
